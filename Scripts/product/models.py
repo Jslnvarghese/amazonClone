bd58d7e8c8e0910aa709dd12f63db31938205c92
Danny Nasra <danina964@gmail.com>
2023-01-22 12:39:15 +0100

    design all product models

--- Scripts/product/models.py
+++ Scripts/product/models.py
@@ -1,7 +1,8 @@
+from django.utils import timezone
 from django.db import models
 from taggit.managers import TaggableManager
 from django.utils.translation import gettext_lazy as _ #as _ , um es als _ zu benutzen
-
+from django.contrib.auth.models import User # um den default User zu importieren und weiter nutzen
 
 PRODUCT_FLAG ={
     ('Sale','Sales'),
@@ -11,18 +12,18 @@ PRODUCT_FLAG ={
 
 # Create your models here.
 class Product(models.Model):
-   name =models.CharField(_('Name'),max_length=150)
-   img= models.ImageField(_('img'),upload_to='products/',default='default.png')
-   flag=models.CharField(_('flag'),max_length=10,choices=PRODUCT_FLAG)
-   price =models.FloatField(_('price'))
-   sku= models.IntegerField(_('sku'))
-   brand = models.ForeignKey('Brand',verbose_name=_('brand'),related_name='product_brand',on_delete=models.CASCADE) #CASCADE delete all the related products ,if the brand is deleted
-   tags = TaggableManager()
-   subtitle = models.TextField(_('subtitle'),max_length=500)
-   description = models.TextField(_('description'),max_length=200)
-
-   def __str__ (self):
-    return self.name
+    name =models.CharField(_('Name'),max_length=150)
+    img= models.ImageField(_('img'),upload_to='products/',default='default.png')
+    flag=models.CharField(_('flag'),max_length=10,choices=PRODUCT_FLAG)
+    price =models.FloatField(_('price'))
+    sku= models.IntegerField(_('sku'))
+    brand = models.ForeignKey('Brand',verbose_name=_('brand'),related_name='product_brand',on_delete=models.CASCADE) #CASCADE delete all the related products ,if the brand is deleted
+    tags = TaggableManager()
+    subtitle = models.TextField(_('subtitle'),max_length=500)
+    description = models.TextField(_('description'),max_length=200)
+    slug = models.SlugField(null=True,blank=True) # um ein link mit dem Title übereinzustimmen also derselbe Name
+    def __str__ (self):
+        return self.name
 '''
 name 
 flag
@@ -36,14 +37,25 @@ description
 '''
 
 class Images(models.Model):
-    pass
+    Product = models.ForeignKey(Product,verbose_name=_('product'),related_name='product_image',on_delete=models.CASCADE)
+    img=models.models.ImageField(_('image'),upload_to='product_images/', height_field=None, width_field=None, max_length=None)
+
+    def __str__(self):
+        return str(self.Product)
 '''
 img
 product: foreignkey
 '''
 
 class Reviews(models.Model):
-    pass
+    Product = models.ForeignKey(Product,verbose_name=_('product'),related_name='product_review',on_delete=models.CASCADE)
+    user= models.ForeignKey(User,verbose_name=_('user'),relateed_name='review_author',on_delete=models.SET_NULL , null=True, blank=True) # um die reviews nicht zu löchen falls den User gelöscht ist
+    comment = models.CharField(_('comment'),max_length=200)
+    rate = models.models.IntegerField(_("rate"))
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return str(self.Product)
 '''
 rate
 user
@@ -53,7 +65,12 @@ product
 '''
 
 class Brand(models.Model):
-    pass
+    name =models.CharField(_('brand'),max_length=30)
+    img=models.models.ImageField(_('image'),upload_to='product_images/', height_field=None, width_field=None, max_length=None)
+
+    def __str__(self):
+        return self.name
+
 '''
 name
 img
