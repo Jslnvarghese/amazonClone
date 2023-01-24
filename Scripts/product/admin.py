8f987ee38b21dd2c46a5bb8252c7e3ea271e0e66
Danny Nasra <danina964@gmail.com>
2023-01-24 21:30:43 +0100

    admin customization to Products

--- Scripts/product/admin.py
+++ Scripts/product/admin.py
@@ -3,7 +3,28 @@ from django.contrib import admin
 # Register your models here.
 from .models import Product, Brand, Reviews,Images
 
-admin.site.register(Product)
+class ProductImagesAdmin(admin.TabularInline):
+    model = Images
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display =['name','brand','price']
+    list_filter = ['brand','price']
+    inlines = [ProductImagesAdmin]
+    search_fields = ['name','price']
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ['user','Product','rate','comment','created_at']
+    list_filter = ['created_at','rate']
+
+
+# class BrandAdmin(admin.ModelAdmin):
+#     list_display = ['name']
+#     inlines = [ProductImagesAdmin]
+
+
+admin.site.register(Product,ProductAdmin)
 admin.site.register(Brand)
-admin.site.register(Reviews)
+admin.site.register(Reviews,ReviewAdmin)
 admin.site.register(Images)
