79f0d69b4257e8917005fc5759a505962872f9bf
Danny Nasra <danina964@gmail.com>
2023-02-02 07:13:58 +0100

    models ,classes Order ,Orderdetailes added

--- Scripts/orders/models.py
+++ Scripts/orders/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
+from django.utils import timezone
+from product import Product
 # Create your models here.
+ORDER_STATUS =(
+    ('Recieved','Recieved'),
+    ('Processed', 'Processed'),
+    ('Shipped', 'Shipped'),
+    ('Delivered','Delivered'),
+)
+
+
+class Order(models.Model):
+    order_code=models.CharField(max_length=10)
+    user = ''
+    orderStatus= models.CharField(max_length=12,choices=ORDER_STATUS,default='Recieved')
+    deliveryDate=models.DateTimeField(null=True,blank=True)
+    orderDate=models.DateTimeField(default=timezone.LocalTimezone)
+
+
+class Orderdetail(models.Model):
+    order=models.ForeignKey(Order,related_name='order_detail',on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,related_name='order_Product',on_delete=models.SET_NULL,null=True,blank=True)
