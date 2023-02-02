b88251fd2447828e03b28fce7b0d31e117537a5d
Danny Nasra <danina964@gmail.com>
2023-02-02 08:28:23 +0100

    add generate_code

--- Scripts/orders/models.py
+++ Scripts/orders/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from product.models import Product
 from django.contrib.auth.models import User
+from utils.generate_code import generate_code
 # Create your models here.
 ORDER_STATUS =(
     ('Recieved','Recieved'),
@@ -12,7 +13,7 @@ ORDER_STATUS =(
 
 
 class Order(models.Model):
-    order_code=models.CharField(max_length=10)
+    order_code=models.CharField(max_length=10,default=generate_code)
     user = models.ForeignKey(User,related_name="user_order",on_delete=models.SET_NULL,null=True,blank=True)
     orderStatus= models.CharField(max_length=12,choices=ORDER_STATUS,default='Recieved')
     deliveryDate=models.DateTimeField(null=True,blank=True)
