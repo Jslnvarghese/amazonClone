349b0732fc2db4254dc12d901e0037c32e853b79
Danny Nasra <danina964@gmail.com>
2023-02-02 07:20:27 +0100

    delete the added class

--- Scripts/orders/models.py
+++ Scripts/orders/models.py
@@ -21,6 +21,3 @@ class Order(models.Model):
 class Orderdetail(models.Model):
     order=models.ForeignKey(Order,related_name='order_detail',on_delete=models.CASCADE)
     product = models.ForeignKey(Product,related_name='order_Product',on_delete=models.SET_NULL,null=True,blank=True)
-
-class Order2(models.Model):
-    text=models.TextField()
